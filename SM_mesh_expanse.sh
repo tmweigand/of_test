8c1d4a3c032d20ef61b4cd148453a676ec340c4c
Timothy Weigand <68024672+tmweigand@users.noreply.github.com>
2025-04-16 17:09:37 -0400

    mesh

--- SM_mesh_expanse.sh
+++ SM_mesh_expanse.sh
@@ -1,15 +1,21 @@
 #!/bin/bash
-#SBATCH --job-name=mp1-1000
-#SBATCH -p batch
-#SBATCH -t 08:00:00
-#SBATCH -n 2000
-#SBATCH --mem-per-cpu=4G
+#SBATCH --job-name="domain_generation"
+#SBATCH --output="domain_generation.%j.%N.out"
+#SBATCH --partition=compute
+#SBATCH --nodes=16
+#SBATCH --ntasks-per-node=128
+#SBATCH --mem=0 
+#SBATCH --account=nca125
+#SBATCH --export=ALL
+#SBATCH -t 12:00:00
+
+# /expanse/lustre/projects/nca125/tweigand
 
 nProcs=2000
 logdir=logs
 
 echo "Sourcing OF-v2406 bashrc"
-source ~/OpenFOAM/OpenFOAM-v2406/etc/bashrc
+source ~/OpenFOAM-v2406/etc/bashrc
 
 mpirun -np ${nProcs} snappyHexMesh -overwrite -parallel > ${logdir}/log.snappyHexMesh
 mpirun -np ${nProcs} checkMesh -parallel > ${logdir}/log.checkMesh
